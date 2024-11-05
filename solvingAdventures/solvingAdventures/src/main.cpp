6941c8593345816ed36a83d73aae1d9eb317a91b
Ulvie Mustafa <UMMustafa23@codingburgas.bg>
2024-11-05 22:54:16 +0200

    Create working environment (Close #7)

---
+++ solvingAdventures/solvingAdventures/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/mainMenu.h"
+using namespace std;
+
+
+int main()
+{
+	mainMenu();
+}
---
+++ solvingAdventures/solvingAdventures/src/mainMenu.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../include/mainMenu.h"
+using namespace std;
+
+void mainMenu()
+{
+	cout << "Menu";
+}
