8cb6bdb4eae5d5a7b57c8eeab3530d62ffd54aca
Martin Dimarov <MNDiamarov23@codingburgas.bg>
2024-11-06 21:11:12 +0200

    Make the menu design

--- solvingAdventures/solvingAdventures/src/mainMenu.cpp
+++ solvingAdventures/solvingAdventures/src/mainMenu.cpp
@@ -2,7 +2,44 @@
 #include "../include/mainMenu.h"
 using namespace std;
 
-void mainMenu()
+void mainMenu() 
 {
-	cout << "Menu";
+
+
+    // Design of Menu
+    cout << "                ----------------------------------------------------------------------------------------------------------------------------------------                " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "                                            ----------------------------------------------------------------------------                                    " << endl;
+    cout << "                                           |                                                                            |                                   " << endl;
+    cout << "                                           |                          Solving adventures                                |                                   " << endl; // title
+    cout << "                                           |                          ------------------                                |                                   " << endl;
+    cout << "                                           |                            In wonderland                                   |                                   " << endl;
+    cout << "                                           |                                                                            |                                   " << endl;
+    cout << "                                            ----------------------------------------------------------------------------                                    " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "                                                                     ----------------------                                                              " << endl;
+    cout << "                                                                    |        Start         |                                                              " << endl; // Start game
+    cout << "                                                                     ----------------------                                                              " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "                                                                     ----------------------                                                              " << endl;
+    cout << "                                                                    |         Story        |                                                             " << endl; // Start story
+    cout << "                                                                     ----------------------                                                              " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "                                                                     ----------------------                                                              " << endl;
+    cout << "                                                                    |         Credits      |                                                             " << endl; // Credits for the developers
+    cout << "                                                                     ----------------------                                                              " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "                                                                                                                                                                        " << endl;
+    cout << "              ----------------------------------------------------------------------------------------------------------------------------------------                          ";
+
+
+
+
+ 
+
 }
